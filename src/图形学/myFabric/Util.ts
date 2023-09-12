a847a9a53b21361b4c6055163f512ae1bef07e53
ljr <1368298676@qq.com>
2023-09-12 16:54:40 +0800

    feat: Util类

---
+++ src/图形学/myFabric/Util.ts
@@ -0,0 +1,20 @@
+export class Util {
+  static createElement<T extends HTMLElement>(type: string = 'div'): T {
+    const el = document.createElement(type) as T;
+    return el;
+  }
+
+  static createCanvasElement(): HTMLCanvasElement {
+    return Util.createElement<HTMLCanvasElement>('canvas');
+  }
+  static addClassname(el: HTMLElement, classname: string | string[]) {
+    if (typeof classname === 'string') classname = [classname];
+    el.classList.add(...classname);
+  }
+
+  static setStyle(el: HTMLElement, props: { [key: string]: string | number }) {
+    Object.entries(props).forEach(([key, value]) => {
+      el.style[key] = value;
+    });
+  }
+}
