bdb5996a3c862e83e6efa798483b3c14f41c3a7a
ljr <1368298676@qq.com>
2023-09-18 11:38:36 +0800

    fix: 修改判断线线相交逻辑

--- src/图形学/myFabric/Intersection.ts
+++ src/图形学/myFabric/Intersection.ts
@@ -24,32 +24,35 @@ export class Intersection {
     this.points = this.points.concat(points);
   }
 
-  static intersectLineLine(a1: Point, a2: Point, b1: Point, b2: Point) {
-    let result: Intersection,
-      a1a2 = a2.subtract(a1),
-      b1b2 = b2.subtract(b1),
-      a1b1 = b1.subtract(a1),
-      ua_t = b1b2.cross(a1b1),
-      ub_t = a1a2.cross(a1b1),
-      u_b = a1a2.cross(b1b2);
+  static intersectLineLine(
+    a1: Point,
+    a2: Point,
+    b1: Point,
+    b2: Point,
+  ): Intersection {
+    let result: Intersection = new Intersection(
+      IntersectionStatus.NoIntersection,
+    );
+    const vectorAB = new Point(a2.x - a1.x, a2.y - a1.y);
+    const vectorAC = new Point(b1.x - a1.x, b1.y - a1.y);
+    const vectorAD = new Point(b2.x - a1.x, b2.y - a1.y);
+    const vectorCD = new Point(b2.x - b1.x, b2.y - b1.y);
+    const vectorCA = new Point(a1.x - b1.x, a1.y - b1.y);
+    const vectorCB = new Point(a2.x - b1.x, a2.y - b1.y);
 
-    if (u_b === 0) {
-      if (ua_t === 0 || ub_t === 0) {
-        result = new Intersection(IntersectionStatus.Coincident);
-      } else {
-        result = new Intersection(IntersectionStatus.Parallel);
-      }
-    } else {
-      let ua = ua_t / u_b,
-        ub = ub_t / u_b;
-      if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
-        result = new Intersection('Intersection');
-        result.points.push(
-          new Point(a1.x + ua * (a2.x - a1.x), a1.y + ua * (a2.y - a1.y)),
-        );
-      } else {
-        result = new Intersection('No Intersection');
-      }
+    const cross1 = vectorAB.cross(vectorAC);
+    const cross2 = vectorAB.cross(vectorAD);
+    const cross3 = vectorCD.cross(vectorCA);
+    const cross4 = vectorCD.cross(vectorCB);
+
+    if (cross1 * cross2 < 0 && cross3 * cross4 < 0) {
+      const t = cross3 / (cross3 - cross4);
+
+      const intersectionX = b1.x + vectorCD.x * t;
+      const intersectionY = b1.y + vectorCD.y * t;
+      const intersectionPoint = new Point(intersectionX, intersectionY);
+      result = new Intersection(IntersectionStatus.Intersection);
+      result.appendPoint(intersectionPoint);
     }
 
     return result;
@@ -59,8 +62,11 @@ export class Intersection {
       length = points.length;
     for (let i = 0; i < length; i++) {
       let b1 = points[i],
-        b2 = points[i + 1],
-        inter = Intersection.intersectLineLine(a1, a2, b1, b2);
+        b2 = points[i + 1];
+      if (!b2) {
+        b2 = points[0];
+      }
+      let inter = Intersection.intersectLineLine(a1, a2, b1, b2);
       result.appendPoints(inter.points);
     }
     if (result.points.length > 0) {
@@ -74,8 +80,11 @@ export class Intersection {
       length = points1.length;
     for (let i = 0; i < length; i++) {
       let a1 = points1[i],
-        a2 = points1[i + 1],
-        inter = Intersection.intersectLinePolygon(a1, a2, points2);
+        a2 = points1[i + 1];
+      if (!a2) {
+        a2 = points1[0];
+      }
+      let inter = Intersection.intersectLinePolygon(a1, a2, points2);
       result.appendPoints(inter.points);
     }
     if (result.points.length > 0) {
@@ -84,5 +93,6 @@ export class Intersection {
 
     return result;
   }
+
   // static intersectPolygonRectangle() {}
 }
