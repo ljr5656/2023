86d402b2d6d289d2ca2e0ff38bb87343def3fbfc
ljr <1368298676@qq.com>
2023-09-18 08:53:28 +0800

    feat: Intersection类 检测物体是否相交

--- src/图形学/myFabric/Intersection.ts
+++ src/图形学/myFabric/Intersection.ts
@@ -34,10 +34,55 @@ export class Intersection {
       u_b = a1a2.cross(b1b2);
 
     if (u_b === 0) {
-      return new Intersection(IntersectionStatus.NoIntersection);
+      if (ua_t === 0 || ub_t === 0) {
+        result = new Intersection(IntersectionStatus.Coincident);
+      } else {
+        result = new Intersection(IntersectionStatus.Parallel);
+      }
+    } else {
+      let ua = ua_t / u_b,
+        ub = ub_t / u_b;
+      if (0 <= ua && ua <= 1 && 0 <= ub && ub <= 1) {
+        result = new Intersection('Intersection');
+        result.points.push(
+          new Point(a1.x + ua * (a2.x - a1.x), a1.y + ua * (a2.y - a1.y)),
+        );
+      } else {
+        result = new Intersection('No Intersection');
+      }
     }
+
+    return result;
   }
-  static intersectLinePolygon(a1: Point, a2: Point, ps: Point[]) {}
-  static intersectPolygonPolygon(ps1: Point[], ps2: Point[]) {}
+  static intersectLinePolygon(a1: Point, a2: Point, points: Point[]) {
+    let result = new Intersection('No Intersection'),
+      length = points.length;
+    for (let i = 0; i < length; i++) {
+      let b1 = points[i],
+        b2 = points[i + 1],
+        inter = Intersection.intersectLineLine(a1, a2, b1, b2);
+      result.appendPoints(inter.points);
+    }
+    if (result.points.length > 0) {
+      result.status = IntersectionStatus.Intersection;
+    }
+
+    return result;
+  }
+  static intersectPolygonPolygon(points1: Point[], points2: Point[]) {
+    let result = new Intersection('No Intersection'),
+      length = points1.length;
+    for (let i = 0; i < length; i++) {
+      let a1 = points1[i],
+        a2 = points1[i + 1],
+        inter = Intersection.intersectLinePolygon(a1, a2, points2);
+      result.appendPoints(inter.points);
+    }
+    if (result.points.length > 0) {
+      result.status = IntersectionStatus.Intersection;
+    }
+
+    return result;
+  }
   // static intersectPolygonRectangle() {}
 }
