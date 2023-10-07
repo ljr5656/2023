ab1b9dfb0031b0829e4804787c42346703fad9e3
ljr5656 <1368298676@qq.com>
2023-10-07 08:50:26 +0800

    test: webgl coloredPoints

--- src/test.ts
+++ src/test.ts
@@ -3,7 +3,11 @@
 // import './图形学/myFabric/test/rect.test';
 // import './图形学/myFabric/test/ellipse.test';
 // import './图形学/myFabric/test/bbox.test';
-import './图形学/myFabric/test/intersection.test';
+// import './图形学/myFabric/test/intersection.test';
 
 // import './图形学/粒子系统/index';
+//#endregion
+
+//#region
+import './图形学/webgl_demo/test';
 //#endregion
